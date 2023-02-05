9235f846d0aa7d1dc5ae5917b214a78b6f47feca
Ezekiel Ogunkunle <bukeze2119@outlook.com>
2023-02-04 20:02:59 -0500

    updated implementation of the Result model that includes the logic to calculate the grade based on the score in the save() method

--- rms/models.py
+++ rms/models.py
@@ -28,8 +28,23 @@ class Result(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     score = models.FloatField()
-    grade = models.CharField(max_length=2)
+    grade = models.CharField(max_length=2, blank=True)
     semester = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.score >= 0 and self.score <= 39:
+            self.grade = 'F'
+        elif self.score >= 40 and self.score <= 49:
+            self.grade = 'D'
+        elif self.score >= 50 and self.score <= 59:
+            self.grade = 'C'
+        elif self.score >= 60 and self.score <= 69:
+            self.grade = 'B'
+        elif self.score >= 70 and self.score <= 100:
+            self.grade = 'A'
+        else:
+            raise ValueError('Score must be between 0 and 100')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.student.name} - {self.course.name}"
